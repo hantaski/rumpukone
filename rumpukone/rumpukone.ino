f5b4e2b2ed47cfd3cd0211907f80c6ff6f30605f
Hannu Taskinen <hannu.taskinen@gmail.com>
2023-10-27 13:39:13 +0300

    sample converter and samples added

--- rumpukone/rumpukone.ino
+++ rumpukone/rumpukone.ino
@@ -4,21 +4,49 @@
 
 // Can be included as many times as necessary, without `Multiple Definitions` Linker Error
 #include "RPi_Pico_TimerInterrupt.h"
-#include "kick.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#include "samples/00.h"
+#include "samples/01.h"
+#include "samples/02.h"
+#include "samples/03.h"
+#include "samples/04.h"
+#include "samples/05.h"
+#include "samples/06.h"
+#include "samples/07.h"
 unsigned int outputPin1 = LED_BUILTIN;
 
 // Init RPI_PICO_Timer
 RPI_PICO_Timer ITimer1(1);
 
 #define TIMER1_INTERVAL_MS    1000
-int i=0;
+int i_0=0;
+int i_1=0;
+int i_2=0;
+int i_3=0;
+int i_4=0;
+int i_5=0;
+int i_6=0;
+int i_7=0;
+int tot=0;
 
 bool TimerHandler1(struct repeating_timer *t)
 {
   (void) t;
-  Serial.println("foo "+i);
-  analogWrite(2,kick[i++]);
-  if (i==kick_len) i=0;
+  tot=(s_00[i_0++]+s_01[i_1++]+s_02[i_2++]+s_03[i_3++]+s_04[i_4++]+s_05[i_5++]+s_06[i_6++]+s_07[i_7++]) >> 3;
+
+  if (i_0==s_00_len) i_0=0;
+  if (i_1==s_01_len) i_1=0;
+  if (i_2==s_02_len) i_2=0;
+  if (i_3==s_03_len) i_3=0;
+  if (i_4==s_04_len) i_4=0;
+  if (i_5==s_05_len) i_5=0;
+  if (i_6==s_06_len) i_6=0;
+  if (i_7==s_07_len) i_7=0;
+
+  analogWrite(2,tot);
+
   return true;
 }
 
---
+++ rumpukone/samples/convert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for a in 00 01 02 03 04 05 06 07
+do
+  sox $a.wav -b 16 -c 1 -r 40000 -e unsigned-integer $a.raw
+  i=$(xxd -c 2 -p ${a}.raw | awk '{ printf "0x%s, ", $1 }'|sed 's/, $//')
+  echo "const unsigned int s_${a}[] = { ${i} };" >${a}.h
+  len=$(grep -o "0x" 00.h |wc -l)
+  echo "int s_${a}_len=${len};" >>${a}.h
+done
